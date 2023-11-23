ba84118a21838ef2b553df307894e7debca8747f
lvfan <1396547078@qq.com>
2023-11-23 18:54:29 +0800

    gulimall测试通过

---
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMermberApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallMermberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GulimallMermberApplication.class, args);
+	}
+
+}
